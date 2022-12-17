739510715fe956fa885b4daeb9323b1522e17ce9
Darek <darek.c65@gmail.com>
2022-12-17 19:25:00 +0100

    first menu

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginright = "250px";
-}
+    if(document.getElementById("mySidebar").style.width === "250px"){
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("main").style.marginRight="0px"
+    }
+    else{
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginRight="250px"
+    }
 
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginright = "0";
 }
